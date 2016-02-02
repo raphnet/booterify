21bd6b4eb81f8ecd8a77b422911c285b47dcdcb8
Raphael Assenat <raph@raphnet.net>
2016-02-02 17:12:05 -0500

    wip

--- bootsector.asm
+++ bootsector.asm
@@ -1,32 +1,30 @@
-org 0h
+org 7c00h
 bits 16
 cpu 8086
 [map symbols map.map]
 
-%define DESTINATION_SEGMENT	07E0H
+%define SECTORS_PER_TRACK	9
+
+%define DESTINATION_SEGMENT	1000H
 %define FIRST_SECTOR		2
 %define FIRST_TRACK			0
-%define SECTOR_SIZE			512
-%define NUM_SECTORS_TO_COPY	payload_size / SECTOR_SIZE
+
+init:
+	jmp 0000:start
+
+sectors_to_copy: db 128,0
+sectors_per_track: db 9
 
 start:
-
-	; Setup
-	;
 	; This boot code is at 0000:7c00 (512 bytes)
-	;
-	; The payload is to be installed in a nice 64K segment
-	; right after this boot code.
-	;
-	; 0000:7e00 or rather 07e0:0000
 	;
 	cli
 
 	; Setup stack pointer at the end of our 512 byte space
 	mov ax, 0
 	mov ds, ax
-	mov ss, ax
-	mov sp, 01ffh
+;	mov ss, ax
+;	mov sp, 07CFEh
 
 	; Trace
 	mov ah, 00Eh
@@ -41,70 +39,52 @@ start:
 	mov es, ax
 	mov bx, 0h
 
-	mov word [sector_count], 0
-	mov cl, FIRST_SECTOR
+	; Setup registers for the loop
 	mov ch, FIRST_TRACK
 	mov dh, 0 ; Side
+	mov cl, FIRST_SECTOR ; the first read start after the loader
+	mov al, [sectors_per_track]
+	sub al, 1 ; Loader sector skipped
+	jmp _same_track
+
 copy_loop:
-
+	call progress
+	xor dh, 1 ; Toggle head
+	jne _same_track
+	inc ch ; Increment track when returning to head 0
+_same_track:
 	push ax
-	push bx
-	push cx
-		mov ah, 00Eh
-		mov bh, 0
-		mov bl, 1
-		mov cx, 1
-
-		mov cx, [sector_count]
-		and cx, 0xf
-		jnz nn
-		mov al, 13
-		int 10h
-		mov al, 10
-		int 10h
-nn:
-		mov al, '.'
-		int 10h
-
-	pop cx
-	pop bx
+	xor ah,ah
+	mov al, [sectors_per_track]
+	cmp word [sectors_to_copy], ax
 	pop ax
-
-	push bx
-	push cx
-	push dx
-		mov al, 1 ; Always read one sector
-		mov ah, 02h ; INT 13H 02H : Read sectors
-		mov dl, 0 ; Todo: Check if we can know which floppy we've been loaded from
-		int 13h ; Read the sectors
-	pop dx
-	pop cx
-	pop bx
+	jg _complete_track ; al stays untouched
+	mov al, [sectors_to_copy]
+_complete_track:
+	mov ah, 02h
+	int 13h
 	jc error
 
-	add bx, SECTOR_SIZE
+	push ax
+	push cx
+		; AL equals the number of sectors that were just read
+		xor ah,ah
+		sub word [sectors_to_copy], ax
 
-	inc word [sector_count]
-	cmp word [sector_count], NUM_SECTORS_TO_COPY
-	je loading_done
+		; Multiply by 512 and increment BX
+		mov cl, 9
+		shl ax, cl
+		add bx, ax
+	pop cx
+	pop ax
 
-	inc cl
-	cmp cl, 9
-	jg _next_side
-	jmp copy_loop
+	; Prepare values for the next phase.
+	mov cl, 1 ; all reads now start at track 1
+	mov al, [sectors_per_track] ; read complete tracks
 
-_next_side:
-	mov cl, 1 ; start at sector 1 on next head
-	inc dh
-	cmp dh, 2
-	jge _next_track
-	jmp copy_loop
+	test word [sectors_to_copy], 0xffff
+	jnz copy_loop
 
-_next_track:
-	inc ch
-	mov cl, 1 ; back to sector 1
-	mov dh, 0 ; back to head 0
-	jmp copy_loop
 
 loading_done:
 	mov ah, 00Eh
@@ -125,6 +105,18 @@ start_payload:
 	; Go!
 	jmp DESTINATION_SEGMENT:0100h
 
+progress:
+	push ax
+	push bx
+	mov ah, 0Eh
+	mov al, '.'
+	mov bh, 0
+	mov bl, 1
+	int 10h
+	pop bx
+	pop ax
+	ret
+
 error:
 	mov al, ah ; Return code from int13h
 	add al, 'A'
@@ -137,18 +129,16 @@ error:
 hang:
 	jmp hang
 
-sector_count: resw 1
-
-section .marker start=0x1FE
+section .marker start=0x7dFE
 	db 0x55, 0xaa
 
 section .data
 
 payload:
 	times 100h db 0x00
-	;incbin "../dosgames/ratillry.com"
+	incbin "../dosgames/ratillry.com"
 	;incbin "/mnt/yggdrasil/games/pc/mspacman/MSPACMAN.COM"
-	incbin "/mnt/yggdrasil/games/pc/pipes/pipes.com"
+	;incbin "/mnt/yggdrasil/games/pc/pipes/pipes.com"
 payload_size: equ $-payload
 
 section .pading start=368639
