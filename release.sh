c5fb7656373e127f63f1a8bf43b3928a8e7bc46f
Raphael Assenat <raph@raphnet.net>
2016-03-20 00:08:28 -0400

    remove building from release script

--- release.sh
+++ release.sh
@@ -8,8 +8,7 @@ echo "Release script for $PREFIX"
 if [ $# -ne 2 ]; then
 	echo "Syntax: ./release.sh version releasedir"
 	echo
-	echo "ex: './release 1.0' will produce $PREFIX-1.0.tar.gz in releasedir out of git HEAD,"
-	echo "untar it, build the firmware and create $PREFIX-1.0.hex in releasedir."
+	echo "ex: './release 1.0' will produce $PREFIX-1.0.tar.gz in releasedir out of git HEAD."
 	exit
 fi
 
@@ -34,14 +33,3 @@ fi
 
 git tag $TAG -f -a
 git archive --format=tar --prefix=$DIRNAME/ HEAD | gzip > $RELEASEDIR/$FILENAME
-
-cd $RELEASEDIR
-tar zxf $FILENAME
-cd $DIRNAME
-make
-cp $HEXFILE ../$PREFIX-$VERSION.hex
-cd ..
-echo
-echo
-echo
-ls -l $PREFIX-$VERSION.*
