f5d18b61491e0ea08805d6ac482f35ad653ca7b3
Raphael Assenat <raph@raphnet.net>
2019-02-05 22:00:38 -0500

    Add a script for building the zip32 archive

---
+++ makewin32archive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function errorExit
+{
+	echo $1
+	exit 1
+}
+
+source version.inc
+
+make -f Makefile.mxe || errorExit "Compilation error"
+
+FILENAME=booterify_win32-$VERSION.zip
+
+[ ! -f $FILENAME ] || errorExit "$FILENAME already exists!"
+
+echo "Preparing $FILENAME"
+zip -r $FILENAME *.exe *.c *.h Makefile* README.md bootsector.* version.inc bochs-config changelog.txt || errorExit "Error building archive. Missing file?"
+
+echo "Done!"
+ls -lh $FILENAME
